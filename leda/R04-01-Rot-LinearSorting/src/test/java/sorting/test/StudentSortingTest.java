ff8fb0b16fc71f408d7132a921ff9ff0a8bb036b
winiciusallan <winiciusab12@gmail.com>
2023-08-29 22:15:37 -0300

    commit

--- leda/R04-01-Rot-LinearSorting/src/test/java/sorting/test/StudentSortingTest.java
+++ leda/R04-01-Rot-LinearSorting/src/test/java/sorting/test/StudentSortingTest.java
@@ -7,7 +7,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import sorting.AbstractSorting;
-import sorting.linearSorting.CountingSort;
+import sorting.linearSorting.ExtendedCountingSort;
 
 public class StudentSortingTest {
 
@@ -41,7 +41,7 @@ public class StudentSortingTest {
 	 * do aluno
 	 */
 	private void getImplementation() {
-		this.implementation = new CountingSort();
+		this.implementation = new ExtendedCountingSort();
 	}
 
 	public void populaVetorTamanhoPar(Integer[] arrayPadrao) {
