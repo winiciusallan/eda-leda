337f989ea731eb650b8a2c096d740d2e4e9476db
122110397 WINICIUS ALLAN BEZERRA DA SILVA <winiciusab12@gmail.com>
2023-09-27 09:24:09 -0300

    adiciona get prime

--- leda/R10-01-Rot-TabelaHash/src/main/java/adt/hashtable/closed/HashtableClosedAddressImpl.java
+++ leda/R10-01-Rot-TabelaHash/src/main/java/adt/hashtable/closed/HashtableClosedAddressImpl.java
@@ -1,8 +1,10 @@
 package adt.hashtable.closed;
 
 import adt.hashtable.hashfunction.HashFunction;
+import adt.hashtable.hashfunction.HashFunctionClosedAddress;
 import adt.hashtable.hashfunction.HashFunctionClosedAddressMethod;
 import adt.hashtable.hashfunction.HashFunctionFactory;
+import util.Util;
 
 public class HashtableClosedAddressImpl<T> extends
 		AbstractHashtableClosedAddress<T> {
@@ -53,14 +55,19 @@ public class HashtableClosedAddressImpl<T> extends
 	 * prime.
 	 */
 	int getPrimeAbove(int number) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		int result = number;
+
+		while (!Util.isPrime(number)) {
+			result++;
+		}
+
+		return result;
 	}
 
 	@Override
 	public void insert(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		int hash = ((HashFunctionClosedAddress<T>) hashFunction).hash(element);
+	
 	}
 
 	@Override
