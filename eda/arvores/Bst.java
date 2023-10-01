196a3bc24f53f0c15a61842c1795f0b191ff928e
Winicius Allan <winiciusab12@gmail.com>
2023-10-01 15:21:37 -0300

    add bst and some methods (insert, search, search, min, max and height)

---
+++ eda/arvores/Bst.java
@@ -0,0 +1,184 @@
+public class Bst {
+
+    private Node root;
+
+    public Bst(Node root) {
+        this.root = root;
+    }
+
+    public boolean isEmpty() {
+        return this.root == null;
+    }
+
+    public void add(int v) {
+
+        Node newNode = new Node(v);
+        if (isEmpty()) { // Se estiver vazio ainda não temos um nó raiz, então adicionamos.
+            this.root = newNode;
+        } else {
+            Node aux = this.root; // Ponteiro começa do nó raiz.
+
+            while (aux != null) {
+
+                if (v < aux.v) { // Se o valor do novo node for menor que o aux (deve ser inserido à esquerda).
+                    if (aux.left == null) { // Se o nó à esquerda for null, deve ser inserido como uma folha.
+                        aux.left = newNode;
+                        newNode.parent = aux;
+                        return;
+                    }
+
+                    aux = aux.left; // Caso não seja null, vai caminhando pra esquerda e faz outras verificaçoes.
+                } 
+
+                if (v > aux.v) {
+                    if (aux.right == null) {
+                        aux.right = newNode;
+                        newNode.parent = aux;
+                        return;
+                    }
+
+                    aux = aux.right;
+                }
+            }
+        }
+    }
+
+    // O(h) onde h é a altura da árvore, já que o elemento terá que ser adicionado como folha.  
+    public void recursiveAdd(int v) {
+        if (isEmpty()) {
+            this.root = new Node(v);
+        } else {
+            recursiveAdd(v, this.root);
+        }
+    }
+
+    private void recursiveAdd(int v, Node current) {
+
+        Node newNode = new Node(v);
+        if (v < current.v) {
+            if (current.left == null) {
+                current.left = newNode;
+                newNode.parent = current;
+                return;
+            }
+
+            recursiveAdd(v, current.left);
+        }
+
+        if (v > current.v) {
+            if (current.right == null) {
+                current.right = newNode;
+                newNode.parent = current;
+                return;
+            }
+
+            recursiveAdd(v, current.right);
+        }
+    }
+
+    // O(h) já que no seu pior caso terá que percorrer todos os ramos da árvore.
+    public Node search(int v) {
+        if (isEmpty()) return null;
+
+        Node current = this.root;
+        while (current != null) {
+            if (current.v == v) return current;
+
+            if (v < current.v) current = current.left;
+
+            if (v > current.v) current = current.right;
+        }
+
+        return null;
+    }
+
+    
+    public Node recursiveSearch(int v) {
+        return recursiveSearch(v, this.root);
+    }
+
+    private Node recursiveSearch(int v, Node current) {
+
+        if (current == null) {
+            return null;
+        }
+
+        if (v == current.v) {
+            return current;
+        }
+
+        if (v < current.v) {
+            return recursiveSearch(v, current.left);
+        } else {
+            return recursiveSearch(v, current.right);
+        }
+    }
+
+    public Node max() {
+        Node current = this.root;
+
+        while (current != null) {
+            current = current.right;
+        }
+
+        return current;
+    }
+
+    public Node recursiveMax() {
+        return recursiveMax(this.root);
+    }
+
+    public Node recursiveMax(Node current) {
+        if (current == null) return null;
+
+        if (current.right == null) return current;
+
+        return recursiveMax(current.right);
+    }
+
+    public Node min() {
+        if (isEmpty()) return null;
+
+        Node current = this.root;
+
+        while (current.left != null) {
+            current = current.left;
+        }
+
+        return current;
+    }
+
+    public Node recursiveMin() {
+        return recursiveMin(this.root);
+    }
+
+    private Node recursiveMin(Node current) {
+        if (current == null) return null;
+
+        if (current.left == null) return current;
+
+        return recursiveMin(current.left);
+    }
+
+    public int height() {
+        return height(this.root);
+    }
+
+    private int height(Node current) {
+        if (current == null) return -1;
+
+        return 1 + Math.max(height(current.right), height(current.left));
+    }
+
+}
+
+class Node {
+    int v;
+    Node left;
+    Node right;
+    Node parent;
+
+    public Node(int v) {
+        this.v = v;
+    }
+}
