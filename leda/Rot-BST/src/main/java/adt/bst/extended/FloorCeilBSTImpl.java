42c3a56a11c5fe18f044e67fd066d8d0a5ed677d
Winicius Allan <winiciusab12@gmail.com>
2023-10-12 20:07:36 -0300

    add bst extended (floor and ceil)

--- leda/Rot-BST/src/main/java/adt/bst/extended/FloorCeilBSTImpl.java
+++ leda/Rot-BST/src/main/java/adt/bst/extended/FloorCeilBSTImpl.java
@@ -1,6 +1,7 @@
 package adt.bst.extended;
 
 import adt.bst.BSTImpl;
+import adt.bst.BSTNode;
 
 /**
  * Note que esta classe estende sua implementacao de BST (BSTImpl).
@@ -11,14 +12,52 @@ public class FloorCeilBSTImpl extends BSTImpl<Integer> implements FloorCeilBST {
 
 	@Override
 	public Integer floor(Integer[] array, double numero) {
-		//TODO Implemente seu codigo aqui
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (array.length > 0) {
+			for (Integer v : array) {
+				this.insert(v);
+			}
+		}
+
+		return floor(null, numero, this.root);
+	}
+
+	private Integer floor(Integer floor, double numero, BSTNode<Integer> node) {
+		if (!node.isEmpty()) {
+			if (numero < node.getData()) {
+				floor = floor(floor, numero, (BSTNode<Integer>) node.getLeft());
+			} else if (numero > node.getData()) {
+				floor = floor(node.getData(), numero, (BSTNode<Integer>) node.getRight());
+			} else {
+				floor = node.getData();
+			}
+		}
+
+		return floor;
 	}
 
 	@Override
 	public Integer ceil(Integer[] array, double numero) {
-		//TODO Implemente seu codigo aqui
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (array.length > 0) {
+			for (Integer v : array) {
+				this.insert(v);
+			}
+		}
+
+		return ceil(null, numero, this.root);
+	}
+
+	private Integer ceil(Integer ceil, double numero, BSTNode<Integer> node) {
+		if (!node.isEmpty()) {
+			if (numero < node.getData()) {
+				ceil = ceil(node.getData(), numero, (BSTNode<Integer>) node.getLeft());
+			} else if (numero > node.getData()) {
+				ceil = ceil(ceil, numero, (BSTNode<Integer>) node.getRight());
+			} else {
+				ceil = node.getData();
+			}
+		}
+
+		return ceil;
 	}
 
 }
