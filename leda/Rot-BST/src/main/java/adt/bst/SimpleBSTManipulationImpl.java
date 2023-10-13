6d8f78af2be52de1a770d7acfb3a32010a6e4dc6
Winicius Allan <winiciusab12@gmail.com>
2023-10-12 21:01:41 -0300

    add bst simple manipulation (equals, similar, order statistic)

--- leda/Rot-BST/src/main/java/adt/bst/SimpleBSTManipulationImpl.java
+++ leda/Rot-BST/src/main/java/adt/bst/SimpleBSTManipulationImpl.java
@@ -10,20 +10,70 @@ public class SimpleBSTManipulationImpl<T extends Comparable<T>> implements Simpl
 
 	@Override
 	public boolean equals(BST<T> tree1, BST<T> tree2) {
-		// TODO Implement this method
-		throw new UnsupportedOperationException("Not implemented yet!");
+		return equals((BSTNode<T>) tree1.getRoot(), (BSTNode<T>) tree2.getRoot());
+	}
+
+	public boolean equals(BSTNode<T> node1, BSTNode<T> node2) {
+		boolean result = false;
+
+		if (node1.isEmpty() && node2.isEmpty()) { // Chegou até o final da verificação; Árvores iguais.
+			result = true;
+		} else if (!node1.isEmpty() && !node2.isEmpty()) {
+			if (node1.equals(node2)) {
+				result = equals((BSTNode<T>) node1.getLeft(), (BSTNode<T>) node2.getLeft()) &&
+						 equals((BSTNode<T>) node2.getRight(), (BSTNode<T>) node2.getRight());
+			}
+		}
+
+		return result;
 	}
 
 	@Override
 	public boolean isSimilar(BST<T> tree1, BST<T> tree2) {
-		// TODO Implement this method
-		throw new UnsupportedOperationException("Not implemented yet!");
+		return isSimilar((BSTNode<T>) tree1.getRoot(), (BSTNode<T>) tree2.getRoot());
 	}
 
+	private boolean isSimilar(BSTNode<T> node1, BSTNode<T> node2) {
+		boolean result = false;
+
+		if (node1.isEmpty() && node2.isEmpty()) {
+			result = true;
+		} else if (!node1.isEmpty() && !node2.isEmpty()) {
+			result = isSimilar((BSTNode<T>) node1.getLeft(), (BSTNode<T>) node2.getLeft()) &&
+					 isSimilar((BSTNode<T>) node1.getRight(), (BSTNode<T>) node2.getRight());
+		}
+
+		return result;
+	}
+	
 	@Override
 	public T orderStatistic(BST<T> tree, int k) {
-		// TODO Implement this method
-		throw new UnsupportedOperationException("Not implemented yet!");
+		T result = null;
+
+		if (k <= tree.size() && k >= 1) {
+			result = orderStatistic((BSTNode<T>) tree.getRoot(), k, 0);
+		}
+
+		return result;
+	}
+
+	private T orderStatistic(BSTNode<T> node, int k, int i) {
+		T result = null;
+
+		if (!node.isEmpty()) {
+			result = orderStatistic((BSTNode<T>) node.getLeft(), k, i);
+			i++;
+
+			if (i == k) { // A estatística de ordem.
+				result = node.getData();
+			}
+
+			if (result == null) { // Não tem como ir mais para a esquerda
+				result = orderStatistic((BSTNode<T>) node.getRight(), k, i);
+			}
+		}
+
+		return result;
 	}
 
 }
