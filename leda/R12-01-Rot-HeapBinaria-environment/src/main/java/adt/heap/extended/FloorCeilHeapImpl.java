26b6c3832392dcb094228f3467b85c8c179ebe7f
Winicius Allan <winiciusab12@gmail.com>
2023-10-19 06:52:06 -0300

    add heap floor and ceil implementation

--- leda/R12-01-Rot-HeapBinaria-environment/src/main/java/adt/heap/extended/FloorCeilHeapImpl.java
+++ leda/R12-01-Rot-HeapBinaria-environment/src/main/java/adt/heap/extended/FloorCeilHeapImpl.java
@@ -12,14 +12,47 @@ public class FloorCeilHeapImpl extends HeapImpl<Integer> implements FloorCeilHea
 
 	@Override
 	public Integer floor(Integer[] array, double numero) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		buildHeap(array);
+
+		return floor(numero, null);
+	}
+
+	private Integer floor(double numero, Integer floor) {
+		Integer root = this.extractRootElement();
+
+		if (root != null) {
+			// O número que eu quero encontrar o floor é maior que a raiz?
+			// 
+			if (numero >= root && (floor == null || root >= floor)) {
+				floor = floor(numero, root);
+			} else {
+				// Em outra chamada na pilha de execução ele vai escolher outra raiz, já que estamos sempre removendo a anterior.
+				floor = floor(numero, floor);
+			}
+		}
+
+		return floor;
 	}
 
 	@Override
 	public Integer ceil(Integer[] array, double numero) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		buildHeap(array);
+
+		return ceil(numero, null); 
+	}
+
+	private Integer ceil(double numero, Integer ceil) {
+		Integer root = this.extractRootElement();
+
+		if (root != null) {
+			if (numero <= root && (ceil == null || root <= ceil)) {
+				ceil = ceil(numero, root);
+			} else {
+				ceil = ceil(numero, ceil);
+			}
+		}
+
+		return ceil;
 	}
 
 }
